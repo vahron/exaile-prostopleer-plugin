97a65ff2b4cce44174d6fd4e15bb02bfea66fc7c
mibbim <webartifex@gmail.com>
2010-08-06 23:36:56 +0600

    исправил кодировку тегов

--- __init__.py
+++ __init__.py
@@ -26,6 +26,24 @@ def _enable(eventname, exaile, nothing):
     global panel
     panel = MyPanel(exaile)
 
+def convert(s):
+    conv = { 
+        'À' : 'А', 'Á' : 'Б', 'Â' : 'В', 'Ã' : 'Г', 'Ä' : 'Д', 'Å' : 'Е',
+        'Æ' : 'Ж', 'Ç' : 'З', 'È' : 'И', 'É' : 'Й', '¨' : 'Ё', 'Ê' : 'К', 
+	'Ë' : 'Л', 'Ì' : 'М', 'Í' : 'Н', 'Î' : 'О', 'Ï' : 'П', 'Ð' : 'Р', 
+	'Ñ' : 'С', 'Ò' : 'Т', 'Ó' : 'У', 'Ô' : 'Ф', 'Õ' : 'Х', 'Ö' : 'Ц', 
+	'×' : 'Ч', 'Ø' : 'Ш', 'Ù' : 'Щ', 'Ú' : 'Ъ', 'Û' : 'Ы', 'Ü' : 'Ь', 
+	'Ý' : 'Э', 'Þ' : 'Ю', 'ß' : 'Я',
+	'à' : 'а', 'á' : 'б', 'â' : 'в', 'ã' : 'г', 'ä' : 'д', 'å' : 'е', 
+	'¸' : 'ё', 'æ' : 'ж', 'ç' : 'з', 'è' : 'и', 'é' : 'й', 'ê' : 'к', 
+	'ë' : 'л', 'ì' : 'м', 'í' : 'н', 'î' : 'о', 'ï' : 'п', 'ð' : 'р', 
+	'ñ' : 'с', 'ò' : 'т', 'ó' : 'у', 'ô' : 'ф', 'õ' : 'х', 'ö' : 'ц', 
+	'÷' : 'ч', 'ø' : 'ш', 'ù' : 'щ', 'ú' : 'ъ', 'û' : 'ы', 'ü' : 'ь', 
+	'ý' : 'э', 'þ' : 'ю', 'ÿ' : 'я'
+    } 
+    for k, v in conv.items(): 
+        s = s.replace(k, v)
+    return s
 
 class ExaileFakePP(object):
     def __init__(self, exaile):
@@ -35,8 +53,12 @@ class ExaileFakePP(object):
         track = player.current
         if track.get_tag_raw('__loc').startswith('http://prostopleer.com/'):
             if not track.get_tag_raw('prostopleer_title'):          # а есть ли сохраненные теги?
-                return
-            title = track.get_tag_raw('prostopleer_title')
-            artist = track.get_tag_raw('prostopleer_artist')
+                title = convert(track.get_tag_raw('title', join = True))
+                artist = convert(track.get_tag_raw('artist', join = True))
+            else:
+                title = track.get_tag_raw('prostopleer_title')
+                artist = track.get_tag_raw('prostopleer_artist')
+            album = convert(track.get_tag_raw('album', join = True))
             track.set_tag_raw('title', title)
             track.set_tag_raw('artist', artist)
+            track.set_tag_raw('album', album)
